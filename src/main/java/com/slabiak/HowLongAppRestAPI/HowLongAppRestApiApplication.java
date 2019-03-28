4cf82f98ef356e6afbadbe3f92f2af2eb4830e05
Tomasz <slabiak.tomasz@gmail.com>
2019-03-28 15:28:48 +0100

    zero configuration, initial commit

---
+++ src/main/java/com/slabiak/HowLongAppRestAPI/HowLongAppRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.slabiak.HowLongAppRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowLongAppRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowLongAppRestApiApplication.class, args);
+	}
+
+}
