6c51eec71630c8ff3b04c78c8aba9f5916fdd3a0
Tomasz <slabiak.tomasz@gmail.com>
2019-04-02 18:17:37 +0200

    change url pattern

--- src/main/java/com/slabiak/HowLongAppRestAPI/controller/ReportController.java
+++ src/main/java/com/slabiak/HowLongAppRestAPI/controller/ReportController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/restaurants/{restaurantId}/reports")
+@RequestMapping("/restaurants/{restaurantId}/reports")
 public class ReportController {
 
     @Autowired
